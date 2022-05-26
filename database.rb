e50c0ceb543ad5d1259732e0a7d1254c27cad366
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-05-26 21:06:12 +0300

    Create a store method in the Database class

--- database.rb
+++ database.rb
@@ -7,4 +7,10 @@ class Database
   def initialize
     @store = PStore.new("notes.store")
   end
+
+  def store(note)
+    @store.transaction do
+      @store[note.id.to_sym] = note
+    end
+  end
 end
